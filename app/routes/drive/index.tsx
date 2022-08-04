a9221782f37db586553c19581b4f5d254e7d445a
behoyh <beshoyhanna@outlook.com>
2022-08-03 23:21:30 -0400

    add storage quota and grid columns

--- app/routes/drive/index.tsx
+++ app/routes/drive/index.tsx
@@ -1,16 +1,18 @@
 import { json, redirect } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
-import { getFiles, auth, deleteFile } from "~/models/drive.server";
+import { getFiles, getStorageQuota, auth, deleteFile } from "~/models/drive.server";
 
 type LoaderData = {
     // this is a handy way to say: "posts is whatever type getPosts resolves to"
     files: Awaited<ReturnType<typeof getFiles>>;
+    storageQuota: Awaited<ReturnType<typeof getStorageQuota>>
 };
 
 export const loader = async () => {
     var client = await auth();
     return json<LoaderData>({
         files: await getFiles(client),
+        storageQuota: await getStorageQuota(client),
     });
 };
 
@@ -23,7 +25,7 @@ export async function action({ request }) {
 }
 
 export default function Posts() {
-    const { files } = useLoaderData() as LoaderData;
+    const { files, storageQuota } = useLoaderData() as LoaderData;
     
     if (!files) {
         return <h1>No Files!</h1>
@@ -31,23 +33,26 @@ export default function Posts() {
 
     return (
         <main>
-            <ul>
+            <div className="grid grid-cols-1 gap-4 text-dark text-center">
+                <h2 className="p-4">{(storageQuota.usage / 1000000).toFixed(2)}MB used of {(storageQuota.limit / 1000000).toFixed(2)}</h2>
+            </div>
+            <div className="grid grid-cols-4 gap-4 font-mono text-dark text-sm text-center font-bold leading-6">
                 {files.map((file) => (
-                    <li key={file.id}>
-                        <span>
+                    <div className="p-4 rounded-lg" key={file.id}>
+                        <div className="block">
                             <img src={file.thumbnailLink} style={{ display: "inline" }}></img>
-                        </span>
-                        <span>
+                        </div>
+                        <div className="inline-block">
                             { file.name } 
-                        </span>
+                        </div>
                         &nbsp;&nbsp;&nbsp;&nbsp;
                         <Form method="post" style={{ display: "inline" }}>
                             <input type="hidden" name="id" value={file.id}/>
                             <button type="submit" name="_action" value="delete" className="text-red-600 underline">Delete</button>
                         </Form>
-                    </li>
+                    </div>
                 ))}
-            </ul>
+            </div>
         </main>
     );
 }
